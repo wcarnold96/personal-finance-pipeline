0a3852120f05b87e3dfded1c7ba65c53dccc859b
Caleb <318292950+wcarnold96@users.noreply.github.com>
2026-08-25 08:06:10 -0400

    sync.py now saves transaction information to a timestamped jsonl file

--- sync.py
+++ sync.py
@@ -1,6 +1,7 @@
 import os
 import plaid
 import json
+from datetime import datetime, timezone
 
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 
@@ -12,31 +13,32 @@ try:
 except:
     print("Unable to load access_tokens")
 
-added = []
-modified = []
-removed = []
-
+ts = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
 for item_id in data:
     has_more = True
-    while has_more:
-        if data[item_id]['cursor']:
-            request = TransactionsSyncRequest(
-                    access_token=data[item_id]['access_token'],
-                    cursor=data[item_id]['cursor']
-            )
-        else:
-            request = TransactionsSyncRequest(
-                    access_token=data[item_id]['access_token']
-            )
-        response = client.transactions_sync(request)
+    with open(f"transactions-{ts}.jsonl", "a") as t:
+        while has_more:
+            if data[item_id]['cursor']:
+                request = TransactionsSyncRequest(
+                        access_token=data[item_id]['access_token'],
+                        cursor=data[item_id]['cursor']
+                )
+            else:
+                request = TransactionsSyncRequest(
+                        access_token=data[item_id]['access_token']
+                )
+            response = client.transactions_sync(request)
 
-        added.extend(response['added'])
-        modified.extend(response['modified'])
-        removed.extend(response['removed'])
+            for change_type in ('added', 'modified', 'removed'):
+                for txn in response[change_type]:
+                    record = {'item_id': item_id, 'change_type': change_type, 'sync_timestamp': ts, 'institution': data[item_id]['institution'], 'transaction': txn.to_dict()}
+                    t.write(json.dumps(record, default=str) + "\n")
 
-        has_more = response['has_more']
+            has_more = response['has_more']
 
-        cursor = response['next_cursor']
-        data[item_id]['cursor'] = cursor
+            cursor = response['next_cursor']
+            data[item_id]['cursor'] = cursor
+
 with open("tokens.json", "w") as f:
     json.dump(data, f, indent=2)
+
